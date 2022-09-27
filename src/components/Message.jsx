a57adbe155c7c0b8ecc3184005fa7cb95aa1b8bc
Manux21 <Manux21@yandex.ru>
2022-09-27 23:42:47 +0300

    added Chat, Input and Messages components

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Message = () => {
   return (
-    <div>
-      
+    <div className='message owner'>
+      <div className="messageInfo">
+        <img src='https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80' alt=""/>
+        <span>Just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img src='https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80' alt=""/>
+      </div>
     </div>
   );
 };
