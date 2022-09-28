2636cc58c8abcf7707d8f4f5f6eafbea96d7b5b4
Manux21 <Manux21@yandex.ru>
2022-09-28 15:19:10 +0300

    connected firebase

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -5,7 +5,7 @@ const Message = () => {
     <div className='message owner'>
       <div className="messageInfo">
         <img src='https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80' alt=""/>
-        <span>Just now</span>
+        <span>just now</span>
       </div>
       <div className="messageContent">
         <p>hello</p>
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBrZBVW9qJl4pf2AMaENUG0hgqnjQVnFVs",
+  authDomain: "first-react-chat.firebaseapp.com",
+  projectId: "first-react-chat",
+  storageBucket: "first-react-chat.appspot.com",
+  messagingSenderId: "909539183748",
+  appId: "1:909539183748:web:8d481a1545369571c246d1"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const auth = getAuth();
