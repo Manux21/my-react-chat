0596d0a8714b44f791dd0bb35480c68925c4e7d4
Manux21 <Manux21@yandex.ru>
2022-10-02 16:10:12 +0300

    fixed avatar, and username display name

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import {signOut} from 'firebase/auth'
 import {auth} from '../firebase'
+import {AuthContext} from "../context/AuthContext";
 
 const Navbar = () => {
+  const {currentUser} = useContext(AuthContext)
+
   return (
     <div className='navbar'>
       <span className='logo'>Lama Chat</span>
       <div className='user'>
-        <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80' alt=''/>
-        <span>John</span>
+        <img src={currentUser.photoURL} alt=''/>
+        <span>{currentUser.displayName}</span>
         <button onClick={()=>signOut(auth)}>logout</button>
       </div>
     </div>
