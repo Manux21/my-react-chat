47984b5082ba3dc7eb21a05829f09ce9b1d12885
Manux21 <Manux21@yandex.ru>
2022-10-02 16:31:05 +0300

    added user search feature

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,18 +1,48 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { collection, query, where, getDocs } from "firebase/firestore";
+import { db } from '../firebase'
 
 const Search = () => {
+
+  const [username, setUsername] = useState('')
+  const [user, setUser] = useState(null)
+  const [err, setErr] = useState(false)
+
+
+  const handleSearch = async () => {
+  const q = query (
+    collection(db, 'users'),
+    where('displayName', '==', username)
+    )
+
+    try {
+
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data())
+      });
+
+    } catch(err) {
+      setErr(true)
+    }
+
+  }
+
+  const handleKey = e => {
+    e.code === 'Enter' && handleSearch()
+  }
   return (
     <div className='search'>
       <div className="searchForm">
-        <input type="text" placeholder='Find a user'/>
+        <input type="text" placeholder='Find a user' onKeyDown={handleKey} onChange={e => setUsername(e.target.value)}/>
       </div>
-
-      <div className="userChat">
-        <img src='https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80' alt=""/>
+      {err && <span>Error. User not found</span>}
+      {user && <div className="userChat">
+        <img src={user.photoURL} alt=""/>
         <div className="userChatInfo">
-          <span>Jane</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
   );
 };
