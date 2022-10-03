a7edd4ad34d3a98e9aa3fd3c9b370c9e8b82aa60
Manux21 <Manux21@yandex.ru>
2022-10-03 15:24:38 +0300

    updated chats

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import {AuthContext} from "../context/AuthContext";
+import {ChatContext} from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({message}) => {
+
+  const {currentUser} = useContext(AuthContext)
+  const {data} = useContext(ChatContext)
+
+
+  console.log(message)
   return (
     <div className='message owner'>
       <div className="messageInfo">
-        <img src='https://images.unsplash.com/photo-1628157588553-5eeea00af15c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2080&q=80' alt=""/>
+        <img
+          src={
+            message.senderId === currentUser.uid
+              ? currentUser.photoURL
+              : data.user.photoURL
+          }
+          alt=""/>
         <span>just now</span>
       </div>
       <div className="messageContent">
